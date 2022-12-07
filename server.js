0d5b9b5c14cd0d103b2379744c678666b85234eb
samwhitford11 <samantha.fichte98@gmail.com>
2022-12-06 19:16:04 -0700

    deployment config

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express') // bring in express to make our app
 const morgan = require('morgan') // nice logger for our request
 const methodOverride = require('method-override') // allows us to override post request from our ejs/forms
 
-const FruitRouter = require('./controllers/fruit')
-const UserRouter = require("./controllers/user")
+
+
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
