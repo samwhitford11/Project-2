48d6f6b20c8d7f2068c8eda941d71a80d01e9532
samwhitford11 <samantha.fichte98@gmail.com>
2022-12-07 20:03:36 -0700

    creates show route

--- controllers/trail.js
+++ controllers/trail.js
@@ -12,12 +12,24 @@ const router = express.Router()
 
 //Index Route
 router.get('/trails', (req, res) => {
+    //Trail is the mongoose model. invoke the .find method from mongoose passing in an empty object to find all docs in database that match Trail models schema
     Trail.find({})
+    //.then gives access to the data found by .find method. pass a callback func to the .then to manipulate the data, trails reps the data
     .then((trails) => {
         console.log(trails, "theses are the trails")
         res.render("trails/index.ejs", {trails});
     });
 });
+
+//Show Route
+router.get('/trails/:id', (req, res) => {
+    const id = req.params.id
+
+    Trail.findById(id, (err, trail) => {
+        res.render('trails/show.ejs', {trail})
+    });
+});
+
 
 
 
--- models/seed.js
+++ models/seed.js
@@ -6,11 +6,11 @@ const Trail = require('./trail')
 mongoose.connection.on('open', () => {
 
     const startingTrails = [
-        {name: "Dirty Dozen", location: "Winter Park", distance: "2,849 ft", priaryDirection: "Downhill", difficulty: "black diamond", isComplete: false },
-        {name: "The Glades", location: "Boulder", distance: "5,165 ft", priaryDirection: "Downhill", difficulty: "blue", isComplete: true },
-        {name: "Aspen Alley", location: "Breckenridge", distance: "1.3 miles", priaryDirection: "Downhill", difficulty: "blue", isComplete: true },
-        {name: "Raleigh Peak", location: "Buffalo Creek", distance: "10 miles", priaryDirection: "Both Directions", difficulty: "Blue", isComplete: false },
-        {name: "Ute Valley", location: "Colorado Springs", distance: "1 mile", priaryDirection: "Both Directions", difficulty: "Green", isComplete: true },
+        {name: "Dirty Dozen", location: "Winter Park", distance: "2,849 ft", primaryDirection: "Downhill", difficulty: "black diamond", isComplete: false },
+        {name: "The Glades", location: "Boulder", distance: "5,165 ft", primaryDirection: "Downhill", difficulty: "blue", isComplete: true },
+        {name: "Aspen Alley", location: "Breckenridge", distance: "1.3 miles", primaryDirection: "Downhill", difficulty: "blue", isComplete: true },
+        {name: "Raleigh Peak", location: "Buffalo Creek", distance: "10 miles", primaryDirection: "Both Directions", difficulty: "Blue", isComplete: false },
+        {name: "Ute Valley", location: "Colorado Springs", distance: "1 mile", primaryDirection: "Both Directions", difficulty: "Green", isComplete: true },
         
     ]
 // Detleing and creating MTB trails
